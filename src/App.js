714ecaf9dbf46d678c9f43d69de61871202d66e9
Pooja-Lohar25 <poojalohar789@gmail.com>
2023-05-20 16:19:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello pooja
+          hello world from <code>src/App.js</code> file
         </p>
         <a
           className="App-link"
