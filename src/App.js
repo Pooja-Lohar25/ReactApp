6d02821c912cdf4c57e73ab4fcbcd06fb0b6f8b4
Pooja-Lohar25 <poojalohar789@gmail.com>
2023-06-08 19:19:38 +0530

    search movies dynamically

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
 
   useEffect(() => {
     setMovies([])
-    searchmovies("spiderman");
+    searchmovies("");
   }, []);
 
   return (
@@ -29,7 +29,10 @@ const App = () => {
       <div className="search">
         <input placeholder="Search" 
         value={searchTerm} 
-        onChange={(e) => setSearchTerm(e.target.value)} />
+        onChange={(e) => {
+          setSearchTerm(e.target.value)
+          searchmovies(e.target.value)
+          }} />
         <img
           src={SearchIcon}
           alt="search"
